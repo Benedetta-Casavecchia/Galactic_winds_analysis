b0f357919abf46bf1d4d6b2e607f421c7f45d008
Benedetta-Casavecchia <81049742+Benedetta-Casavecchia@users.noreply.github.com>
2021-09-13 10:42:01 +0200

    Create density_downthebarrel.py

---
+++ density_downthebarrel.py
@@ -0,0 +1,32 @@
+def density_downthebarrel(i, name_file, input_directory, rho_0, mu, L_0):
+
+#Make down-the-barrel columndensity plot
+    
+    m_p = 1.67*10**(-24)
+    
+    rho, _, _, _, _, _, _, _, tr1 = pload_vtk(name_file, input_directory)
+    
+    n_cgs    = rho*rho_0*tr1/(mu*m_p)
+    n_cgs_3d = np.reshape(n_cgs, (96, 192, 96))
+    N_mc     = np.sum(n_cgs_3d*(L_0/8), axis = 1)
+    
+    ## CREATE PLOTS
+    
+    x1  = np.linspace(-60, 60, 96)
+    y1  = np.linspace(-60, 60, 96)
+    x,y = np.meshgrid(x1,y1)
+    
+    magma  = plt.get_cmap('magma_r')
+    plt.figure(figsize=(6,5))
+    plt.pcolormesh(x, y, N_mc, norm=matplotlib.colors.LogNorm(vmax = 10**20, vmin = 10), cmap = magma, shading = 'nearest')
+    plt.colorbar(label = r'$N_{mc} \ [cm^{-3}]$')
+    plt.xticks(fontsize=13)
+    plt.yticks(fontsize=13)
+    plt.xlabel(r'$x \ [pc]$', fontsize=13)
+    plt.ylabel(r'$z \ [pc]$', fontsize=13)
+    plt.axis('equal')
+    #if(name_file == 'data.0012.vtk'):
+        #plt.figtext(0.17, 0.83, r'MHD2AlTurb-rad', fontsize = 13)
+   #plt.show()
+    
+    #return N_mc
